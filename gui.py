62cd2505990ae1e46b8492db47a74b6266930204
Vinit Gohil <vgohil@me.com>
2023-09-07 16:42:28 +0100

    Added theme, time and handled edit and complete errors

--- gui.py
+++ gui.py
@@ -1,25 +1,46 @@
 from modules import utils
 import PySimpleGUI as sg
+import time
 
+# Apply GUI Theme
+sg.theme("SystemDefault")
+
+
+# Define labels
+timeLabel = sg.Text("", key="clock")
 label = sg.Text("Type in a To-Do Task")
 
 # Add an input text instance and add a key for the to-do task to add
 input_box  = sg.InputText(tooltip="Enter a to-do task", key="TaskItem")
+create_file_box = sg.InputText("Create a TaskFile: ", key="FILENAME")
+
+create_file_button = sg.Button("Create File")
 add_button = sg.Button("Add")
 
+
+
 list_box = sg.Listbox(values=utils.read_taskfile(fileName="todoapptask.txt"), key='TaskITEMS',
-                      enable_events=True, size=[45, 10])
+                      enable_events=True, size=[45, 20])
 
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 appWindow = sg.Window("My To-do App",
-                      layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                      layout=[[timeLabel],
+                              [label],
+                              [input_box, add_button],
+                              [list_box, edit_button, complete_button],
+                              [exit_button]],
                       font=('Helvetica', 12))
+
+
 while True:
-    task, values = appWindow.read()
-    print(1, task)
-    print(2, values)
-    print(3, values['TaskITEMS'])
+    task, values = appWindow.read(timeout=200)
+    appWindow['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+    # print(1, task)
+    # print(2, values)
+    # print(3, values['TaskITEMS'])
 
     match task:
         case "Add":
@@ -30,14 +51,31 @@ while True:
             appWindow['TaskITEMS'].update(values=todos)
 
         case "Edit":
-            edit_task = values['TaskITEMS'][0]
-            new_task = values['TaskItem'].capitalize()
+            try:
+                edit_task = values['TaskITEMS'][0]
+                new_task = values['TaskItem'].capitalize()
 
-            todos = utils.read_taskfile(fileName="todoapptask.txt")
-            index = todos.index(edit_task)
-            todos[index] = new_task
-            utils.write_taskfile(todos, fileName="todoapptask.txt")
-            appWindow['TaskITEMS'].update(values=todos)
+                todos = utils.read_taskfile(fileName="todoapptask.txt")
+                index = todos.index(edit_task)
+                todos[index] = new_task
+                utils.write_taskfile(todos, fileName="todoapptask.txt")
+                appWindow['TaskITEMS'].update(values=todos)
+            except IndexError as e:
+                sg.Popup("Please select an Item for edit", font=('Helvetica', 12))
+
+        case "Complete":
+            try:
+                complete_task = values['TaskITEMS'][0]
+                todos = utils.read_taskfile(fileName="todoapptask.txt")
+                todos.remove(complete_task)
+                utils.write_taskfile(todos, fileName="todoapptask.txt")
+                appWindow['TaskITEMS'].update(values=todos)
+                appWindow['TaskItem'].update(value="")
+            except IndexError as e:
+                sg.Popup("Please select an Item to complete", font=('Helvetica', 12))
+
+        case "Exit":
+            break
 
         case 'TaskITEMS':
             appWindow['TaskItem'].update(value=values['TaskITEMS'][0])
