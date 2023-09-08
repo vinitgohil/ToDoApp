df758646e3c2543eb1922b0eb345c705927f5354
Vinit Gohil <vgohil@me.com>
2023-09-08 16:08:52 +0100

    Added new addTask function to add new tasks to web task list

--- web.py
+++ web.py
@@ -3,6 +3,13 @@ from modules import utils
 
 taskList = utils.read_taskfile()
 
+def addTask():
+    task = st.session_state['newTask']
+    taskList.append(task.capitalize())
+    utils.write_taskfile(taskList)
+
+
+
 st.title("My Todo App")
 st.subheader("Web Application to add tasks to your to-do list")
 st.text("This is to manage your tasks and your productivity")
@@ -10,4 +17,5 @@ st.text("This is to manage your tasks and your productivity")
 for task in taskList:
     st.checkbox(task)
 
-st.text_input(label="Enter a Task to do:", placeholder="Add task...")
+st.text_input(label="Enter a Task to do:", placeholder="Add task...",
+              on_change=addTask, key='newTask')
